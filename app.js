d13ddcf6f6467c061be11e85918c6e3e50b33424
Ulises Gaston Soto <ulyssesgaston-97@hotmail.com>
2024-11-17 07:35:24 -0300

    Game structure changed to work with buttons

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const validOptions = ["Paper", "Rock", "Scissor"];
-
 let exit = false;
-
-let playerChoice;
-
+let playerChoice = "";
+let computerChoise = "";
 let playerScore = 0;
 let computerScore = 0;
 
-function checkPlayerChoice(playerChoice) {
-  for (let i of validOptions) {
-    if (i === playerChoice) return true;
-  }
-  return false;
-}
+const buttons = document.querySelectorAll("button");
+const btnPlay = document.getElementById("btnPlay");
+const result = document.getElementById("result");
+const pScore = document.getElementById("playerScore");
+const cScore = document.getElementById("computerScore");
+
+pScore.textContent = `${playerScore}`;
+cScore.textContent = `${computerScore}`;
 
 function getComputerChoise() {
   let max = 2;
@@ -20,67 +20,42 @@ function getComputerChoise() {
   return validOptions[Math.floor(Math.random() * (max - min + 1) + min)];
 }
 
-function playerMove() {
-  playerChoice = prompt("Choose Paper Rock or Scissor");
-  while (!checkPlayerChoice(playerChoice)) {
-    alert("Invalid option");
-    playerChoice = prompt("Choose Paper Rock or Scissor");
+function play(playerChoice) {
+  let computerChoise = getComputerChoise();
+  if (
+    (playerChoice === "Paper" && computerChoise === "Paper") ||
+    (playerChoice === "Rock" && computerChoise === "Rock") ||
+    (playerChoice === "Scissor" && computerChoise === "Scissor")
+  ) {
+    result.textContent = "No winner";
+    console.log("No winner");
   }
-  return playerChoice;
-}
-
-function validPlayAgain() {
-  let valid = false;
-  let yn;
-  while (!valid) {
-    yn = prompt("¿Do you want to play again? y/n");
-    if (yn === "y" || yn === "n") valid = true;
-    else alert("Choose y or n");
-  }
-  return yn;
-}
-
-function play() {
-  if (playerChoice === "Paper" && getComputerChoise() === "Paper")
-    alert("No winner");
-  if (playerChoice === "Rock" && getComputerChoise() === "Paper") {
-    alert("¡Computer wins!");
-    computerScore++;
-  }
-  if (playerChoice === "Scissor" && getComputerChoise() === "Paper") {
-    alert("¡Player wins!");
+  if (
+    (playerChoice === "Scissor" && computerChoise === "Paper") ||
+    (playerChoice === "Rock" && computerChoise === "Scissor") ||
+    (playerChoice === "Paper" && computerChoise === "Rock")
+  ) {
+    result.textContent = "Player wins";
+    console.log("Player wins");
     playerScore++;
   }
-  if (playerChoice === "Paper" && getComputerChoise() === "Rock") {
-    alert("¡Player wins!");
-    playerScore++;
-  }
-  if (playerChoice === "Rock" && getComputerChoise() === "Rock")
-    alert("No winner");
-  if (playerChoice === "Scissor" && getComputerChoise() === "Rock") {
-    alert("¡Computer wins!");
+  if (
+    (playerChoice === "Rock" && computerChoise === "Paper") ||
+    (playerChoice === "Scissor" && computerChoise === "Rock") ||
+    (playerChoice === "Paper" && computerChoise === "Scissor")
+  ) {
+    result.textContent = "Computer wins";
+    console.log("Computer wins");
     computerScore++;
   }
-  if (playerChoice === "Paper" && getComputerChoise() === "Scissor") {
-    alert("¡Computer wins!");
-    computerScore++;
-  }
-  if (playerChoice === "Rock" && getComputerChoise() === "Scissor") {
-    alert("¡Player wins!");
-    playerScore++;
-  }
-  if (playerChoice === "Scissor" && getComputerChoise() === "Scissor")
-    alert("¡No winner!");
+  pScore.textContent = `${playerScore}`;
+  cScore.textContent = `${computerScore}`;
 }
 
-playerChoice = playerMove();
-play();
-
-while (!exit) {
-  if (validPlayAgain() === "y") {
-    playerChoice = playerMove();
-    play();
-  } else exit = true;
-}
-
-alert("GoodBye");
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    playerChoice = btn.textContent;
+    play(playerChoice);
+    playerChoice = "";
+  });
+});
